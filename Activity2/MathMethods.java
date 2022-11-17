313d0462bca9dd989e07a1e9ab948e161944dd81
tosinalao <79558547+tosinalao@users.noreply.github.com>
2022-11-16 22:00:26 -0500

    Update MathMethods.java

--- Activity2/MathMethods.java
+++ Activity2/MathMethods.java
@@ -1,1 +1,20 @@
+package Activity2;
+
+public class MathMethods {
+    public static void abs() {
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.abs(4.7));
+        System.out.println(Math.abs(3)); 
+    }
+
+    public static void acos() {
+        System.out.println(Math.acos(0.64));
+        System.out.println(Math.acos(-0.4));
+        System.out.println(Math.acos(0));
+        System.out.println(Math.acos(1));
+        System.out.println(Math.acos(-1));
+        System.out.println(Math.acos(2)); 
+    }
+
+}
 
