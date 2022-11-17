b44bf86fbc006b74b902867159c6063690370d37
tosinalao <79558547+tosinalao@users.noreply.github.com>
2022-11-16 22:01:35 -0500

    Create MethodDemo.java

---
+++ Activity2/MethodDemo.java
@@ -0,0 +1,21 @@
+package Activity2;
+
+public class MethodDemo {
+    public static void main(String [] args) {
+       
+        System.out.println("String methods");
+        StringMethods.charAt();
+        StringMethods.compareTo();
+        StringMethods.concat();
+        StringMethods.contains();
+
+        System.out.println("Math methods");
+        MathMethods.abs();
+        MathMethods.acos();
+
+
+
+
+
+    }
+}
